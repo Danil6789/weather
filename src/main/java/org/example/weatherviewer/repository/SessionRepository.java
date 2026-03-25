dd52022a6a7285a366a91f9c794b3398f64fb698
Danil6789 <sotnikov6789@gmail.com>
2026-03-25 23:14:04 +0400

    feat: add session repository

---
+++ src/main/java/org/example/weatherviewer/repository/SessionRepository.java
@@ -0,0 +1,35 @@
+package org.example.weatherviewer.repository;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.example.weatherviewer.entity.Session;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class SessionRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void save(Session session){
+        sessionFactory.getCurrentSession().persist(session);
+    }
+
+    public void delete(Session session){
+        sessionFactory.getCurrentSession().remove(session);
+    }
+
+    public Optional<Session> findById(UUID id){
+        return Optional.ofNullable(sessionFactory.getCurrentSession().get(Session.class, id));
+    }
+
+    public int deleteByExpiresAtBefore(LocalDateTime now){
+        return sessionFactory.getCurrentSession()
+                .createMutationQuery("DELETE FROM Session WHERE expires_at < :now")
+                .setParameter("now", now).executeUpdate();
+    }
+}

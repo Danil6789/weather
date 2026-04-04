438190cfbdf3a36d860aa6c2e5da95ecce764373
Danil6789 <sotnikov6789@gmail.com>
2026-04-04 18:06:58 +0400

    refactor: reversed List<ForecastDto> for home page

--- src/main/java/org/example/weatherviewer/service/forecast/ForecastService.java
+++ src/main/java/org/example/weatherviewer/service/forecast/ForecastService.java
@@ -6,6 +6,7 @@ import org.example.weatherviewer.dto.location.LocationDto;
 import org.example.weatherviewer.dto.weather.WeatherResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +19,12 @@ public class ForecastService {
     public List<ForecastDto> getUserForecasts(Long userId) {
         List<LocationDto> locations = locationService.getUserLocations(userId);
 
-        return locations.stream().map(location -> {
-            WeatherResponse weather = weatherService.getWeather(location.getLatitude(), location.getLongitude());
-            return new ForecastDto(location.getId(), weather);
-        }).collect(Collectors.toList());
+        return locations.stream()
+                .sorted(Comparator.comparing(LocationDto::getId).reversed())
+                .map(location -> {
+                    WeatherResponse weather = weatherService.getWeather(location.getLatitude(), location.getLongitude());
+                    return new ForecastDto(location.getId(), weather);
+                })
+                .collect(Collectors.toList());
     }
 }
